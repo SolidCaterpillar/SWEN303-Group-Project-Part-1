0c47e51b42c6e05d8a97f16b51371e1abcc7ab05
ZianPayne <zian@payne.name>
2024-05-22 16:27:13 +1200

    Adding Card Styles

--- flutter_prototype/lib/screens/expense_screen.dart
+++ flutter_prototype/lib/screens/expense_screen.dart
@@ -8,11 +8,6 @@ class ExpenseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Expenses',
-            style: TextStyle(color: AppColors.textPrimary)),
-        backgroundColor: AppColors.primary,
-      ),
       body: SingleChildScrollView(
         child:
             _makeMain(), // Single method to make the main content of expenses
@@ -33,75 +28,113 @@ class ExpenseScreen extends StatelessWidget {
       ),
     );
   }
-Row _makeFutureButton() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      ElevatedButton(
-        onPressed: () {
-          // Handle button press here
-        },
-        child: Text(
-          'Past',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 10,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w600,
+
+  Row _makeFutureButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            // Handle button press here
+          },
+          child: Text(
+            'Past',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 10,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Color(0xFF9A9A9A), // Button color
+            fixedSize: Size(170, 40), // Button size
           ),
         ),
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Color(0xFF9A9A9A), // Button color
-          fixedSize: Size(170, 40), // Button size
-        ),
-      ),
-      ElevatedButton(
-        onPressed: () {
-          // Handle button press here
-        },
-        child: Text(
-          'Future',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 10,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w600,
+        ElevatedButton(
+          onPressed: () {
+            // Handle button press here
+          },
+          child: Text(
+            'Future',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 10,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(170, 40), // Button size
           ),
         ),
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Color(0xFF9A9A9A), // Button color
-          fixedSize: Size(170, 40), // Button size
+      ],
+    );
+  }
+
+  Padding _makeExpenseCard() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 340,
+        height: 60,
+        decoration: ShapeDecoration(
+          color: Color.fromARGB(255, 185, 92, 92),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
         ),
       ),
-    ],
-  );
-}
+    );
+  }
+
+  Container _makeExpenseSection() {
+    return Container(
+      width: 340,
+      decoration: ShapeDecoration(
+        color: Color.fromARGB(255, 112, 48, 48),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Today',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          _makeExpenseCard(),
+          _makeExpenseCard(),
+        ],
+      ),
+    );
+  }
+
   Container _makeExpenseSheet() {
     return Container(
       width: 340,
-      height: 600,
-      margin: const EdgeInsets.only(top: 20),
+      margin: const EdgeInsets.only(top: 20, bottom: 20),
       decoration: ShapeDecoration(
+        color: Color.fromARGB(255, 134, 128, 128),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(18),
         ),
       ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            width: 340,
-            height: 600,
-            decoration: ShapeDecoration(
-              color: Color(0xFFE0E0E0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(11),
-              ),
-            ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: _makeExpenseSection(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: _makeExpenseSection(),
           ),
         ],
       ),
