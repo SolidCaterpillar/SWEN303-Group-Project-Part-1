88433e21fa064d3c6ea147b6748ad0554c66110c
ZianPayne <zian@payne.name>
2024-05-22 15:52:04 +1200

    Adding Backplate + Buttons

--- flutter_prototype/lib/screens/expense_screen.dart
+++ flutter_prototype/lib/screens/expense_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../core/constants.dart';
 
 class ExpenseScreen extends StatelessWidget {
@@ -12,220 +13,95 @@ class ExpenseScreen extends StatelessWidget {
             style: TextStyle(color: AppColors.textPrimary)),
         backgroundColor: AppColors.primary,
       ),
-      body: SingleChildScrollView( 
-        child: _makeExpenseSheet(),
+      body: SingleChildScrollView(
+        child:
+            _makeMain(), // Single method to make the main content of expenses
       ),
     );
   }
 
+  Center _makeMain() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _makeFutureButton(),
+          _makeExpenseSheet(),
+        ],
+      ),
+    );
+  }
+Row _makeFutureButton() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      ElevatedButton(
+        onPressed: () {
+          // Handle button press here
+        },
+        child: Text(
+          'Past',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 10,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Color(0xFF9A9A9A), // Button color
+          fixedSize: Size(170, 40), // Button size
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          // Handle button press here
+        },
+        child: Text(
+          'Future',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 10,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Color(0xFF9A9A9A), // Button color
+          fixedSize: Size(170, 40), // Button size
+        ),
+      ),
+    ],
+  );
+}
   Container _makeExpenseSheet() {
     return Container(
       width: 340,
-      height: 160,
+      height: 600,
+      margin: const EdgeInsets.only(top: 20),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18),
+        ),
+      ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Stack(
-            children: [
-              Positioned(
-                left: 0,
-                top: 0,
-                child: Container(
-                  width: 340,
-                  height: 160,
-                  decoration: ShapeDecoration(
-                    color: Color(0xFFB9B9B9),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5)),
-                  ),
-                ),
+          Container(
+            width: 340,
+            height: 600,
+            decoration: ShapeDecoration(
+              color: Color(0xFFE0E0E0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(11),
               ),
-              Positioned(
-                left: 15.95,
-                top: 0,
-                child: SizedBox(
-                  width: 160.53,
-                  height: 20,
-                  child: Text(
-                    'This Week',
-                    style: TextStyle(
-                      color: Color(0xFF343434),
-                      fontSize: 16,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w600,
-                      height: 0,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 9.97,
-                top: 20,
-                child: Container(
-                  width: 319.06,
-                  height: 60,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 319.06,
-                        height: 60,
-                        decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 319.06,
-                              height: 60,
-                              decoration: ShapeDecoration(
-                                color: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 126.74,
-                              height: 30,
-                              child: Text(
-                                'Statement',
-                                style: TextStyle(
-                                  color: Color(0xFF343434),
-                                  fontSize: 13,
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.w600,
-                                  height: 0,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 102.81,
-                              height: 30,
-                              child: Text(
-                                'Amount',
-                                textAlign: TextAlign.right,
-                                style: TextStyle(
-                                  color: Color(0xFF343434),
-                                  fontSize: 13,
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.w600,
-                                  height: 0,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 126.74,
-                              height: 30,
-                              child: Text(
-                                'Date',
-                                style: TextStyle(
-                                  color: Color(0xFF343434),
-                                  fontSize: 10,
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.w600,
-                                  height: 0,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 9.97,
-                top: 90,
-                child: Container(
-                  width: 319.06,
-                  height: 60,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 319.06,
-                        height: 60,
-                        decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 319.06,
-                              height: 60,
-                              decoration: ShapeDecoration(
-                                color: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 126.74,
-                              height: 30,
-                              child: Text(
-                                'Statement',
-                                style: TextStyle(
-                                  color: Color(0xFF343434),
-                                  fontSize: 13,
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.w600,
-                                  height: 0,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 102.81,
-                              height: 30,
-                              child: Text(
-                                'Amount',
-                                textAlign: TextAlign.right,
-                                style: TextStyle(
-                                  color: Color(0xFF343434),
-                                  fontSize: 13,
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.w600,
-                                  height: 0,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 126.74,
-                              height: 30,
-                              child: Text(
-                                'Date',
-                                style: TextStyle(
-                                  color: Color(0xFF343434),
-                                  fontSize: 10,
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.w600,
-                                  height: 0,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ],
       ),
